8a5f72c12c2cc680b2f91df8a6a248c2b282c9c8
richardli03 <rli@olin.edu>
2023-11-17 13:46:13 -0500

    add launch folder to ros package

---
+++ search-and-rescue/launch/launch.py
@@ -0,0 +1,95 @@
+"""
+Sample invocation:
+
+ros2 launch neato_node2 bringup_multi.py host:=192.168.16.59 robot_name:=a udp_video_port:=5002 udp_sensor_port:=7777'
+"""
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import IncludeLaunchDescription, GroupAction
+from launch_ros.actions import Node, PushRosNamespace
+from launch.substitutions import LaunchConfiguration
+import os
+
+
+def generate_launch_description():
+    use_udp = DeclareLaunchArgument("use_udp", default_value="true")
+    host_1 = DeclareLaunchArgument("host1", default_value="192.168.16.115")
+    host_2 = DeclareLaunchArgument("host2", default_value="192.168.17.205")
+
+    udp_video_port = LaunchConfiguration("udp_video_port")
+    udp_video_port_command = DeclareLaunchArgument(
+        "udp_video_port", default_value="5002"
+    )
+
+    udp_sensor_port = DeclareLaunchArgument("udp_sensor_port", default_value="7777")
+    robot_name = LaunchConfiguration("robot_name")
+
+    robot_name_command = DeclareLaunchArgument("robot_name", default_value="")
+    # interfaces_launch_file_dir = os.path.join(
+    #     get_package_share_directory("neato2_gazebo"), "launch"
+    # )
+    robot_1_name = "alice"
+    robot_2_name = "billy"
+    robot_3_name = "carson"
+
+    return LaunchDescription(
+        [
+            use_udp,
+            host_1,
+            host_2,
+            udp_sensor_port,
+            udp_video_port_command,
+            robot_name_command,
+            GroupAction(
+                actions=[
+                    # ONE OF THESE PER NEATO
+                    IncludeLaunchDescription(
+                        PythonLaunchDescriptionSource(
+                            "bringup_multi.py"
+                            # os.path.join(
+                            #     interfaces_launch_file_dir, "bringup_multi.py"
+                            # )  # not sure if this path is correct, but uh
+                        ),
+                        launch_arguments={
+                            "robot_name": robot_1_name,
+                            "host": host_1,  # insert correct IP
+                            "udp_video_port": 5001,
+                            "udp_sensor_port": 5002,
+                        }.items(),
+                    ),
+                    # ONE OF THESE PER NEATO
+                    IncludeLaunchDescription(
+                        PythonLaunchDescriptionSource(
+                            "bringup_multi.py"
+                            # os.path.join(
+                            #     interfaces_launch_file_dir, "bringup_multi.py"
+                            # )  # not sure if this path is correct, but uh
+                        ),
+                        launch_arguments={
+                            "robot_name": robot_2_name,
+                            "host": host_2,  # insert correct IP
+                            "udp_video_port": 5003,
+                            "udp_sensor_port": 5004,
+                        }.items(),
+                    ),
+                    # ONE OF THESE PER NEATO
+                    # IncludeLaunchDescription(
+                    #     PythonLaunchDescriptionSource(
+                    #         os.path.join(
+                    #             interfaces_launch_file_dir, "/bringup_multi.py"
+                    #         )  # not sure if this path is correct, but uh
+                    #     ),
+                    #     launch_arguments={
+                    #         "robot_name": robot_3_name,
+                    #         "host": 111111, #insert correct host
+                    #         "udp_video_port": 5001,
+                    #         "udp_sensor_port": 5002,
+                    #     }.items(),
+                    # ),
+                ]
+            ),
+        ]
+    )
