69894f34d13284099d0c8b9873fd9d1218635ba1
richardli03 <rli@olin.edu>
2023-11-28 13:51:23 -0500

    adding so i can review commit history if i need this, but deleting afterwards

--- search-and-rescue/launch/launch.py
+++ search-and-rescue/launch/launch.py
@@ -16,28 +16,53 @@ import os
 
 def generate_launch_description():
     use_udp = DeclareLaunchArgument("use_udp", default_value="true")
-    host_1 = DeclareLaunchArgument("host1", default_value="192.168.16.115")
-    host_2 = DeclareLaunchArgument("host2", default_value="192.168.16.62")
-    udp_video_port_1 = DeclareLaunchArgument("udp_video_port_2", default_value="5011")
-    udp_video_port_2 = DeclareLaunchArgument("udp_video_port_1", default_value="5013")
-    # udp_video_port_1 = DeclareLaunchArgument("udp_video_port_1", default_value="5011")
-    # udp_video_port_2 = DeclareLaunchArgument("udp_video_port_2", default_value="5013")
-    # host_1 = "192.168.16.115"
-    # host_2 = "192.168.16.62"
+    host_1 = DeclareLaunchArgument("host1", default_value="192.168.16.93")
+    host_2 = DeclareLaunchArgument("host2", default_value="192.168.16.95")
 
+    udp_video_port_1 = LaunchConfiguration("udp_video_port_1")
+    udp_video_port_2 = LaunchConfiguration("udp_video_port_2")
+    gscam_config_1 = DeclareLaunchArgument(
+        "gscam_config",
+        default_value=[
+            "udpsrc port=",
+            udp_video_port_1,
+            " ! application/x-rtp, payload=96 ! rtpjitterbuffer ! rtph264depay ! avdec_h264  ! videoconvert",
+        ],
+    )
+    gscam_config_2 = DeclareLaunchArgument(
+        "gscam_config",
+        default_value=[
+            "udpsrc port=",
+            udp_video_port_2,
+            " ! application/x-rtp, payload=96 ! rtpjitterbuffer ! rtph264depay ! avdec_h264  ! videoconvert",
+        ],
+    )
+    udp_video_port_1_command = DeclareLaunchArgument(
+        "udp_video_port_1", default_value="5011"
+    )
+    udp_video_port_2_command = DeclareLaunchArgument(
+        "udp_video_port_2", default_value="5013"
+    )
     # udp_video_port = LaunchConfiguration("udp_video_port")
     # udp_video_port_command = DeclareLaunchArgument(
     #     "udp_video_port", default_value="5002"
     # )
 
-    udp_sensor_port = DeclareLaunchArgument("udp_sensor_port", default_value="7777")
-    robot_name = LaunchConfiguration("robot_name")
+    udp_sensor_port_1 = DeclareLaunchArgument("udp_sensor_port_1", default_value="7777")
+    udp_sensor_port_2 = DeclareLaunchArgument("udp_sensor_port_2", default_value="7778")
+    robot_name_1 = LaunchConfiguration("robot_name_1")
+    robot_name_1_command = DeclareLaunchArgument(
+        "robot_name", default_value=f"{robot_1_name}"
+    )
+    robot_name_2 = LaunchConfiguration("robot_name_2")
+    robot_name_2_command = DeclareLaunchArgument(
+        "robot_name", default_value=f"{robot_2_name}"
+    )
 
-    robot_name_command = DeclareLaunchArgument("robot_name", default_value="")
     interfaces_launch_file_dir = os.path.join(
         get_package_share_directory("search-and-rescue"), ""
     )
-    print(interfaces_launch_file_dir)
+
     robot_1_name = "alice"
     robot_2_name = "billy"
 
@@ -48,11 +73,14 @@ def generate_launch_description():
             use_udp,
             host_1,
             host_2,
-            udp_sensor_port,
-            udp_video_port_1,
-            udp_video_port_2,
-            # udp_video_port_command,
-            robot_name_command,
+            udp_sensor_port_1,
+            udp_sensor_port_2,
+            udp_video_port_1_command,
+            udp_video_port_2_command,
+            gscam_config_1,
+            gscam_config_2,
+            robot_name_1_command,
+            robot_name_2_command,
             GroupAction(
                 actions=[
                     # ONE OF THESE PER NEATO
@@ -65,10 +93,10 @@ def generate_launch_description():
                         ),
                         launch_arguments={
                             "robot_name": robot_1_name,
-                            "host": LaunchConfiguration("host1"),  # insert correct IP
-                            # "host": "192.168.16.115",
-                            "udp_video_port": LaunchConfiguration("udp_video_port_1"),
-                            "udp_sensor_port": "5002",
+                            "host": host_1,  # insert correct IP
+                            "udp_video_port": udp_video_port_1_command,
+                            "udp_sensor_port": udp_sensor_port_1,
+                            "gscam_config": gscam_config_1,
                         }.items(),
                     ),
                     # ONE OF THESE PER NEATO
@@ -81,10 +109,10 @@ def generate_launch_description():
                         ),
                         launch_arguments={
                             "robot_name": robot_2_name,
-                            "host": LaunchConfiguration("host2"),  # insert correct IP
-                            # "host": "192.168.16.62",
-                            "udp_video_port": LaunchConfiguration("udp_video_port_2"),
-                            "udp_sensor_port": "5004",
+                            "host": host_2,  # insert correct
+                            "udp_video_port": udp_video_port_2_command,
+                            "udp_sensor_port": udp_sensor_port_2,
+                            "gscam_config": gscam_config_2,
                         }.items(),
                     ),
                     # ONE OF THESE PER NEATO
