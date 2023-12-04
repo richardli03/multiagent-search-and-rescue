030080124e3937b546864818c84a13ea4765c73c
Anmol Sandhu <anmolrattansandhu@gmail.com>
2023-12-03 21:59:18 -0500

    added map yaml generation

--- maps/map_gen.py
+++ maps/map_gen.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import cv2
 import numpy as np
+import yaml
 
 def vertical_split_map_gen(map_dim, num_splits=2):
     # generate map with vertical split
@@ -10,10 +11,28 @@ def vertical_split_map_gen(map_dim, num_splits=2):
         map[:, i*split_width] = 0
     return map
 
+def yaml_dict(map_name):
+    # generate yaml dictionary
+    yaml_dict = {
+        'image': map_name,
+        'mode': 'trinary',
+        'resolution': 0.05,
+        'origin': [-12.5, -25.0, 0.0],
+        'negate': 0,
+        'occupied_thresh': 0.65,
+        'free_thresh': 0.25
+    }
+    return yaml_dict
+
 new_map = vertical_split_map_gen((500,1000), 5)
 
-cv2.imwrite('new_map.pgm', new_map)
+map_file_name = 'new_map.pgm'
 
-newer_map = cv2.imread('new_map.pgm', cv2.IMREAD_GRAYSCALE)
+cv2.imwrite(map_file_name, new_map)
+
+with open(map_file_name.split(".")[0] + ".yaml", 'w') as outfile:
+    yaml.dump(yaml_dict(map_file_name), outfile, default_flow_style=None)
+
+newer_map = cv2.imread(map_file_name, cv2.IMREAD_GRAYSCALE)
 cv2.imshow('newer_map', newer_map)
 cv2.waitKey(0)
