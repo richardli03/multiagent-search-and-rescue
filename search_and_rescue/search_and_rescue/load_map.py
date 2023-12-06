772a19d71edc8efb81ff291355b8dd670991ac99
Anmol Sandhu <37765094+AnmolRattanSingh@users.noreply.github.com>
2023-12-06 16:49:11 -0500

    Merged Richard's work with map server (#2)
    * started work on map generation
    
    * added pgm map image generation
    
    * added map yaml generation
    
    * merged map server into launch files + added maps
    
    * FIX run_loop print statement
    
    ---------
    
    Co-authored-by: richardli03 <rli@olin.edu>

---
+++ search_and_rescue/search_and_rescue/load_map.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+
+""" This is the starter code for the robot localization project """
+
+import rclpy
+from threading import Thread
+from rclpy.time import Time
+from rclpy.node import Node
+from std_msgs.msg import Header
+import math
+import time
+import numpy as np
+from .occupancy_field import OccupancyField
+
+
+class LoadMap(Node):
+    """The class that represents a Particle Filter ROS Node
+    Attributes list:
+        base_frame: the name of the robot base coordinate frame (should be "base_footprint" for most robots)
+        map_frame: the name of the map coordinate frame (should be "map" in most cases)
+        odom_frame: the name of the odometry coordinate frame (should be "odom" in most cases)
+        scan_topic: the name of the scan topic to listen to (should be "scan" in most cases)
+        n_particles: the number of particles in the filter
+        d_thresh: the amount of linear movement before triggering a filter update
+        a_thresh: the amount of angular movement before triggering a filter update
+        pose_listener: a subscriber that listens for new approximate pose estimates (i.e. generated through the rviz GUI)
+        particle_pub: a publisher for the particle cloud
+        last_scan_timestamp: this is used to keep track of the clock when using bags
+        scan_to_process: the scan that our run_loop should process next
+        occupancy_field: this helper class allows you to query the map for distance to closest obstacle
+        transform_helper: this helps with various transform operations (abstracting away the tf2 module)
+        particle_cloud: a list of particles representing a probability distribution over robot poses
+        current_odom_xy_theta: the pose of the robot in the odometry frame when the last filter update was performed.
+                               The pose is expressed as a list [x,y,theta] (where theta is the yaw)
+        thread: this thread runs your main loop
+    """
+
+    def __init__(self):
+        super().__init__("pf")
+        self.base_frame = "base_footprint"  # the frame of the robot base
+        self.map_frame = "map"  # the name of the map coordinate frame
+        self.odom_frame = "odom"  # the name of the odometry coordinate frame
+        self.scan_topic = "scan"  # the topic where we will get laser scans from
+        timer_period = 1
+        self.occupancy_field = OccupancyField(self)
+        self.timer = self.create_timer(timer_period, self.run_loop)
+
+    def loop_wrapper(self):
+        """This function takes care of calling the run_loop function repeatedly.
+        We are using a separate thread to run the loop_wrapper to work around
+        issues with single threaded executors in ROS2"""
+        while True:
+            self.run_loop()
+            time.sleep(0.1)
+
+    def get_map(self):
+        return self.occupancy_field.map
+
+    def run_loop(self):
+        print(self.get_map().data)
+
+    def scan_received(self, msg):
+        self.last_scan_timestamp = msg.header.stamp
+        # we throw away scans until we are done processing the previous scan
+        # self.scan_to_process is set to None in the run_loop
+        if self.scan_to_process is None:
+            self.scan_to_process = msg
+
+
+def main(args=None):
+    rclpy.init()
+    n = LoadMap()
+
+    n.get_map().data
+    rclpy.spin(n)
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
