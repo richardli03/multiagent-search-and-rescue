d21e619076cb7ad0a1ec4f915e9d0c7c5fb7b1a2
Anmol Sandhu <anmolrattansandhu@gmail.com>
2023-12-06 17:36:34 -0500

    removed LoadMap Node and changed it to a class

--- search_and_rescue/setup.py
+++ search_and_rescue/setup.py
@@ -23,8 +23,6 @@ setup(
     entry_points={
         "console_scripts": [
             "multimove = search_and_rescue.move_multi:main",
-            "load_map = search_and_rescue.load_map:main",
-            "occupancy_field = search_and_rescue.occupancy_field:main",
         ],
     },
 )
