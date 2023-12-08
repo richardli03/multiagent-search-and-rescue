cdbe92fb521ec5e73c8a2bb4cf5519faea60c1eb
An Grocki <angrocki02@gmail.com>
2023-12-08 11:52:27 -0500

    Add PID controll and debug angles

--- search_and_rescue/search_and_rescue/lawnmower.py
+++ search_and_rescue/search_and_rescue/lawnmower.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from std_msgs.msg import Header
-from odom_helper import euler_from_quaternion
+from search_and_rescue.odom_helper import euler_from_quaternion, angle_diff, distance_diff
 from enum import Enum
 import math 
 import time
@@ -40,16 +40,19 @@ class Lawnmower(Node):
         self.next_y = 0
         self.max_x = 2
         self.max_y = 5
-        self.linear_speed = 0.5 # CHANGE VALUE 
-        self.angular_speed = 0.5
-        self.init_angle = 0
+        self.linear_speed = 0.3 # CHANGE VALUE 
+        self.angular_speed = 0.1
         self.current_angle = 0
+        self.next_angle = 0
         ## orienting robot in direction of most weighted/closest objects 
         timer_period = 1
         self.threshold = .1
         self.timer = self.create_timer(timer_period, self.run_loop)
         self.object_x = 3
         self.object_y = 20
+        self.distance_diff = self.max_x
+        self.angle_diff = math.pi / 2 
+
 
     def process_odom(self, odom):
         self.odom = odom 
@@ -59,21 +62,13 @@ class Lawnmower(Node):
         Moves robot straight
         '''
         self.move.angular.z = 0.0
-        self.move.linear.x = self.linear_speed
+        self.move.linear.x = self.linear_speed * self.distance_diff
         self.publisher.publish(self.move)
-    def turn_right(self):
+    def turn(self):
         '''
-        Move right 
+        turn based on angle diff 
         '''
-        self.move.angular.z = self.angular_speed * -1
-        self.move.linear.x = 0.0 
-        self.publisher.publish(self.move)
-
-    def turn_left (self):
-        '''
-        Turn left
-        '''
-        self.move.angular.z = self.angular_speed
+        self.move.angular.z = self.angular_speed * self.angle_diff
         self.move.linear.x = 0.0 
         self.publisher.publish(self.move)
 
@@ -88,8 +83,14 @@ class Lawnmower(Node):
         """
         Publish message to main brain 
         """
+        self.move.angular.z = 0.0
+        self.move.linear.x = 0.0 
+        self.publisher.publish(self.move)
         print("Send coordiante to main brain, activating other robots to move to locaization")
     def complete_search(self):
+        self.move.angular.z = 0.0
+        self.move.linear.x = 0.0 
+        self.publisher.publish(self.move)
         print("Send message that area assigned is searched")
     def choose_state(self): 
 
@@ -99,28 +100,37 @@ class Lawnmower(Node):
         if self.next_y >= self.max_y:
             self.state = State.COMPLETE_SERACH
         # Search Loop
-        if self.state == State.MOVE_STRAIGHT_X_RIGHT and self.current_x >= self.max_x: 
+        if self.state == State.MOVE_STRAIGHT_X_RIGHT and self.distance_diff < self.threshold:
+            print(self.distance_diff)
             print("SWITCH TO TURN LEFT 1")
             self.state = State.TURN_lEFT_1
-        elif self.state == State.TURN_lEFT_1 and self.current_angle >= 90: 
-            print("SWITCH TO TURN MOVE STRAIGHT Y")
+            self.next_angle = self.current_angle - 90
+            print(f"Next angle: {self.next_angle * 180 / math.pi}")
+        elif self.state == State.TURN_lEFT_1 and self.angle_diff < 0 : 
+            print("SWITCH TO TURN MOVE STRAIGHT Y")        
+        elif self.state == State.MOVE_STRAIGHT_Y_LEFT and self.distance_diff < self.threshold: 
             self.state = State.MOVE_STRAIGHT_Y_LEFT
             self.next_y = self.current_y + NEOTO_LENGTH
-        elif self.state == State.MOVE_STRAIGHT_Y_LEFT and self.current_y >= self.next_y: 
             print("SWITCH TURN LEFT 2")
             self.state = State.TURN_lEFT_2
-        elif self.state == State.TURN_lEFT_2 and self.current_angle <= 0:
+            self.next_angle = self.current_angle + math.pi/2 
+            print(f"Next angle: {self.next_angle * 180 / math.pi}")
+        elif self.state == State.TURN_lEFT_2 and self.angle_diff < self.threshold:
             print("SWITCH TURN MOVE STRAIGHT X LEFT ")
             print(f"Current Angle: {self.current_angle} --SWITCH")
             self.state = State.MOVE_STRAIGHT_X_LEFT
-        elif self.state == State.MOVE_STRAIGHT_X_LEFT and self.current_x <= self.init_x: 
+        elif self.state == State.MOVE_STRAIGHT_X_LEFT and self.distance_diff < self.threshold: 
             self.state = State.TURN_RIGHT_1
-        elif self.state == State.TURN_RIGHT_1 and self.current_angle <= 90: 
+            self.next_angle = self.current_angle + math.pi/2 
+            print(f"Next angle: {self.next_angle * 180 / math.pi}")
+        elif self.state == State.TURN_RIGHT_1 and self.angle_diff < self.threshold: 
             self.state = State.MOVE_STRAIGHT_Y_RIGHT
             self.next_y = self.current_y + NEOTO_LENGTH
-        elif self.state == State.MOVE_STRAIGHT_Y_RIGHT and self.current_y >= self.next_y: 
+        elif self.state == State.MOVE_STRAIGHT_Y_RIGHT and self.distance_diff < self.threshold: 
             self.state = State.TURN_RIGHT_2
-        elif self.state == State.TURN_RIGHT_2 and self.current_angle <= 0:
+            self.next_angle = self.current_angle + math.pi/2 
+            print(f"Next angle: {self.next_angle * 180 / math.pi}")
+        elif self.state == State.TURN_RIGHT_2 and self.angle_diff < self.threshold:
             self.state = State.MOVE_STRAIGHT_X_RIGHT
 
     def run_loop(self):
@@ -135,26 +145,42 @@ class Lawnmower(Node):
         self.current_y = new_pose.pose.pose.position.y
         print(f"pos: {self.current_x}, {self.current_y}\n")
         _, _, yaw_z  = euler_from_quaternion(new_pose.pose.pose.orientation.x,new_pose.pose.pose.orientation.y, new_pose.pose.pose.orientation.z, new_pose.pose.pose.orientation.w)
-        self.current_angle = yaw_z * 180 / math.pi
-        print(f"angle: {self.current_angle}")
+        self.current_angle = yaw_z 
+        print(f"angle: {self.current_angle * 180 / math.pi}")
         self.choose_state()
         print(self.state)
         match self.state:
             case State.MOVE_STRAIGHT_X_LEFT:
+                self.distance_diff = distance_diff(self.current_x, self.init_x) 
+                print(f"distance diff: {self.distance_diff}")
                 self.move_straight()
             case State.MOVE_STRAIGHT_X_RIGHT:
+                self.distance_diff = distance_diff(self.current_x, self.max_x) 
+                print(f"distance diff: {self.distance_diff}")
                 self.move_straight()
             case State.TURN_RIGHT_1:
-                self.turn_right()
+                self.angle_diff = angle_diff(self.current_angle, self.next_angle)
+                print(f"angle diff: {self.angle_diff * 180 / math.pi}")
+                self.turn()
             case State.TURN_lEFT_1:
-                self.turn_left()
+                self.angle_diff = angle_diff(self.current_angle, self.next_angle)
+                print(f"angle diff: {self.angle_diff * 180 / math.pi}")
+                self.turn()
             case State.TURN_RIGHT_2:
-                self.turn_right()
+                self.angle_diff = angle_diff(self.current_angle, self.next_angle)
+                print(f"angle diff: {self.angle_diff * 180 / math.pi}")
+                self.turn()
             case State.TURN_lEFT_2:
-                self.turn_left()
+                self.angle_diff = angle_diff(self.current_angle, self.next_angle)
+                print(f"angle diff: {self.angle_diff * 180 / math.pi}")
+                self.turn()
             case State.MOVE_STRAIGHT_Y_LEFT:
+                self.distance_diff = distance_diff(self.current_y, self.next_y)
+                print(f"distance diff: {self.distance_diff}")
                 self.move_straight()
             case State.MOVE_STRAIGHT_Y_RIGHT:
+                self.distance_diff = distance_diff(self.current_y, self.next_y)
+                print(f"distance diff: {self.distance_diff}")
                 self.move_straight()   
             case State.FOUND_OBJECT: 
                 self.find_object()
