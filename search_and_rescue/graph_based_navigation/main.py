8f01b28e23e78e14eacaeea761884946d0aa1a1a
Anmol Sandhu <anmolrattansandhu@gmail.com>
2023-12-15 12:11:34 +0530

    added docstrings

--- search_and_rescue/graph_based_navigation/main.py
+++ search_and_rescue/graph_based_navigation/main.py
@@ -4,14 +4,35 @@ import cv2
 from multiastar import multi_agent_a_star
 import matplotlib.pyplot as plt
 
+
 def modify_map(arr):
+    """
+    Modifies the input array to create openings in the wall.
+
+    Parameters:
+    arr (numpy.ndarray): The input array representing the map.
+
+    Returns:
+    numpy.ndarray: The modified array.
+    """
     # create openings in the wall
     arr[10:20, 200] = 255
     arr[60:70, 200] = 255
 
     return arr
 
+
 def colors_to_rgb(clr):
+    """
+    TODO: Find a better way to do this.
+    Converts color abbreviations to their corresponding RGB values.
+
+    Parameters:
+    clr (str): The color abbreviation.
+
+    Returns:
+    tuple: The corresponding RGB values.
+    """
     if clr == 'r':
         return (255, 0, 0)
     elif clr == 'b':
@@ -30,7 +51,18 @@ def colors_to_rgb(clr):
         return (255, 255, 255)
     return (0, 0, 0)
 
+
 def image_to_graph(image_path):
+    """
+    Converts an image to a graph where each white pixel is a node. 
+    Avoids black pixels (obstacles).
+
+    Parameters:
+    image_path (str): The path to the image file.
+
+    Returns:
+    networkx.Graph: The resulting graph.
+    """
     arr = cv2.imread(image_path)
     arr = cv2.cvtColor(arr, cv2.COLOR_BGR2GRAY)
 
@@ -50,20 +82,16 @@ def image_to_graph(image_path):
 
     return graph
 
-def a_star_search(graph, start, goal):
-    return nx.astar_path(graph, start, goal)
 
 MAP_PATH = '../maps/graph_nav.pgm'
 
 graph = image_to_graph(MAP_PATH)
 
-# multi-agent A*
+# define the start and goal nodes for each agent
 agents_start = [(15, 10), (55, 10), (45, 10)]
 agents_goal = [(55, 210), (15, 210), (35, 210)]
 
 paths = multi_agent_a_star(graph, agents_start, agents_goal)
-
-print(paths)
 
 markers = ['o', 'x', 'v', '^', '<', '>', 's', 'p', '*', 'h', 'H', 'D', 'd']
 colors = ['r', 'b', 'k', 'c', 'm', 'y', 'g', 'w']
@@ -79,8 +107,10 @@ for agent_id, path in paths.items():
 plt.imshow(arr)
 
 for agent in range(len(agents_start)):
-    plt.scatter(agents_start[agent][1], agents_start[agent][0], c=colors[agent], marker=markers[agent], label=f'Agent {agent} Start')
-    plt.scatter(agents_goal[agent][1], agents_goal[agent][0], c=colors[agent], marker=markers[agent], label=f'Agent {agent} Goal')
+    plt.scatter(agents_start[agent][1], agents_start[agent][0],
+                c=colors[agent], marker=markers[agent], label=f'Agent {agent} Start')
+    plt.scatter(agents_goal[agent][1], agents_goal[agent][0],
+                c=colors[agent], marker=markers[agent], label=f'Agent {agent} Goal')
 
 plt.legend()
 plt.show()
